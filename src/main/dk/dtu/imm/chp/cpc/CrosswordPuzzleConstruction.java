08512b0e3a2cef0a72199198338f78b04163bd03
Wojciech Pawlak <wojciechpawlak@gmail.com>
2012-11-06 09:48:17 +0100

    Added exception handling if the file not found or specified. Added printing of the result to the main class.

--- src/main/dk/dtu/imm/chp/cpc/CrosswordPuzzleConstruction.java
+++ src/main/dk/dtu/imm/chp/cpc/CrosswordPuzzleConstruction.java
@@ -9,13 +9,18 @@ public class CrosswordPuzzleConstruction {
 
 	public static void main(String[] args) {
 		try {
-			
-			decoder = Decoder.getInstance();
-			decoder.decode(args[0]);
-			algo = HeuristicAlgorithm.getInstance();
-			Board initialBoard = new Board();
-			algo.runAlgorithm(initialBoard);
-		
+			if (!args[0].equals("")) {
+				decoder = Decoder.getInstance();
+				decoder.decode(args[0]);
+				algo = HeuristicAlgorithm.getInstance();
+				Board initialBoard = new Board();
+				Board result = algo.runAlgorithm(initialBoard);
+				System.out.println(result);
+			}
+		} catch (NullPointerException e) {
+			System.out.println("File does not exist");
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Path to file is not specfied");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
